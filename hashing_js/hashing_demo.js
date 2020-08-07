f55ba353fc458e2003f0daeadee66828e394bfd9
mightguy <mikel_gjergji@my.uri.edu>
2020-08-07 10:38:15 -0400

    Initial Demo for hashing using permutations and xors

--- hashing_js/hashing_demo.js
+++ hashing_js/hashing_demo.js
@@ -2,10 +2,19 @@ class Hash {
 
     constructor(){
         this.encoding = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789. ";
+        this.pause = false;
     }
 
     async log(message){
         document.getElementById("encryption-log").innerHTML += message + '\n\n';
+    }
+    
+    async charToBits(character){
+        var binaryString = this.encoding.indexOf(character).toString(2);
+        while(binaryString.length < 6){
+            binaryString = "0" + binaryString;
+        }
+        return binaryString;
     }
 
     async pauseSleep(){
@@ -22,49 +31,87 @@ class Hash {
     }
     
     async resetGraphics(){
-        var lines = document.querySelectorAll('[id^="line"]');
-        for(var i = 0; i < lines.length; i++){
-            lines[i].style.stroke = "rgb(0,0,0)";
+        document.getElementById('arrows').innerHTML = "";
+        
+        var bits = document.querySelectorAll('[id*="-bit-"]');
+        for(var i = 0; i < bits.length; i++){
+            bits[i].innerHTML = "";
         }
         
-        var preBits = document.querySelectorAll('[id^="pre-expander"]');
-        var postBits = document.querySelectorAll('[id^="post-expander"]');
-        var xoredKeyBits = document.querySelectorAll('[id^="xored-key-bit"]');
-        
-        for(var i = 0; i < postBits.length; i++){
-            postBits[i].innerHTML = "";
-            xoredKeyBits[i].innerHTML = "";
-        }
-        
-        for(var i = 0; i < preBits.length; i++){
-            preBits[i].innerHTML = "";
-        }
-
     }
 
-    async xor(bits1, bits2, index){
-        var xored = "";
-        for(var i = 0; i < bits1.length; i++){
-            if (bits1[i] != bits2[i]){
-                xored += "1";
+    async drawLineToBit(top_bit, bottom_bit, svg, index, bit_position){
+        var xorLine = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+        xorLine.setAttribute('id', 'prior-' + index + '-line-' + bit_position);
+        xorLine.setAttribute('x1', top_bit.offsetLeft + 10);
+        xorLine.setAttribute('x2', bottom_bit.offsetLeft + 10);
+        xorLine.setAttribute('y1', top_bit.offsetTop + 20);
+        xorLine.setAttribute('y2', bottom_bit.offsetTop);
+        xorLine.setAttribute("stroke", "black");
+        xorLine.setAttribute("stroke-width", "1.5");
+        svg.appendChild(xorLine);
+        await this.sleep()
+    }
+
+    async xor(index, permutation){
+        
+        var returnValue = "";
+        var bits1 = await this.charToBits(document.getElementById("prior-label-" + index).innerHTML[1]);
+        var bits2 = await this.charToBits(document.getElementById("char-label-" + index).innerHTML[1]);
+        
+        for(var bit = 1; bit <= bits1.length; bit++){
+            var prior = document.getElementById("prior-" + index + "-bit-" + bit);
+            var ch = document.getElementById("char-" + index + "-bit-" + bit);
+            prior.innerHTML = bits1[bit-1];
+            ch.innerHTML = bits2[bit-1];
+            this.sleep()
+        }
+
+        for(var bit = 1; bit <= bits1.length; bit++){
+            
+            // get bits we are xoring
+            var prior = document.getElementById("prior-" + index + "-bit-" + bit);
+            var ch = document.getElementById("char-" + index + "-bit-" + bit);
+            // get bit we are storing (permuted)
+            var xor = document.getElementById("xored-" + index + "-bit-" + permutation[bit-1]);
+            
+            // get svg to draw lines dynamically
+            var svg = document.getElementById("arrows");
+            
+            await this.drawLineToBit(prior, ch, svg, index, bit);
+
+            await this.drawLineToBit(ch, xor, svg, index, bit);
+            if(prior.innerHTML != ch.innerHTML){
+                xor.innerHTML = "1";
+                returnValue += "1";
             }
             else{
-                xored += "0";
+                xor.innerHTML = "0";
+                returnValue += "0";
             }
         }
-        await this.log("Exclusive OR on " + bits1 + " and " + bits2);
-        return xored;
+        await this.log("XORed " + bits1 + " and " + bits2 + " using permutation " + permutation.toString());
+        return this.encoding[parseInt(returnValue, 2)];
     }
 
-    async charToBits(character){
-        var binaryString = this.encoding.indexOf(character).toString(2);
-        while(binaryString.length < 6){
-            binaryString = "0" + binaryString;
+    async visualization(permutations, initial_value, userInputString){
+        var returnString = ""
+        for(var i = 0; i < permutations.length; i++){
+            document.getElementById("prior-label-" + (i+1)).innerHTML = '"' + initial_value[i] + '"';
+            document.getElementById("char-label-" + (i+1)).innerHTML = '"' + userInputString[i] + '"';
+            var result = await this.xor(i+1, permutations[i]);
+            document.getElementById("xored-label-" + (i+1)).innerHTML = '"' + result + '"';
+            await this.sleep();
+            returnString += result;
         }
-        return binaryString;
+
+        await this.resetGraphics();
+
+        return returnString;
+        
     }
 
-
+}
 
 var hasher = new Hash();
 
@@ -73,33 +120,42 @@ function pauseFunction(){
     var playOrPause = document.getElementById("pause-button").innerHTML;
 
     if(playOrPause == "Pause"){
-        des.pause = true;
+        hasher.pause = true;
         document.getElementById("pause-button").innerHTML = "Resume";
     }
 
     else{
-        des.pause = false;
+        hasher.pause = false;
         document.getElementById("pause-button").innerHTML = "Pause";
     }
 }
 
-async function encryptFunction(){
+async function hashFunction(){
     var userInput = document.getElementById("input-text");
     document.getElementById("encryption-log").innerHTML = "";
     document.getElementById("output-text").innerHTML = "";
 
-    while((userInput.value.length % 4) == 1){
+    while(!((userInput.value.length % 4) == 0)){
         userInput.value += " ";
     }
 
-    var key = document.getElementById("key-input").value;
+    var initial_value = document.getElementById("key-input").value;
+
+    var permutations = ["463512", "132465", "653124", "361245"];
+    for (var i = 0; i < 4; i++){
+        permutations[i] = document.getElementById("permutation-" + (i+1)).value;
+    }
+    
 
     while(userInput.value != ""){
-        var userInputString = userInput.value.slice(0,2);
-        userInput.value = userInput.value.slice(2);
-        document.getElementById("output-text").innerHTML += await hasher.xor(userInputString, rounds, key);
+        var userInputString = userInput.value.slice(0,4);
+        userInput.value = userInput.value.slice(4);
+        initial_value = await hasher.visualization(permutations, initial_value, userInputString);
     }
+
+    document.getElementById("output-text").innerHTML = initial_value;
+
 }
 
 document.getElementById("pause-button").addEventListener("click", pauseFunction);
-document.getElementById("hash-button").addEventListener("click", encryptFunction);
+document.getElementById("hash-button").addEventListener("click", hashFunction);
